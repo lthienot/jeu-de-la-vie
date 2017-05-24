ff76fda4791a9db2431927c81b7d241c6d1f966c
Lucile Thienot <lthienot@emi.u-bordeaux1.fr>
2017-05-24 10:59:37 +0200

    curve 1

--- fichiers/experiences/lancer-expe.sh
+++ fichiers/experiences/lancer-expe.sh
@@ -13,11 +13,11 @@ OUTPUT="$(echo $EXE | tr -d ' ')"
 for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
 }
 
-execute -v 3
-execute -v 4
-execute -v 5
-execute -v 6
-
+# execute -v 3
+# execute -v 4
+# execute -v 5
+# execute -v 6
+execute -v 7
 
 
 
