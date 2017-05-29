9735b3837d402ca5a25ede3df57125baeffe5712
Jonathan Aucourt <jaucourt@emi.u-bordeaux1.fr>
2017-05-29 17:32:42 +0200

    tracer-speedUP.R avec doit d'execution

--- fichiers/experiences/lancer-expe.sh
+++ fichiers/experiences/lancer-expe.sh
@@ -5,7 +5,7 @@ ITE=$(seq 10) # nombre de mesures
   
 THREADS=$(seq 2 2 24) # nombre de threads
 
-PARAM="-n -i 100 -s 4096 -a" # parametres commun à toutes les executions 
+PARAM="-n -i 1000 -s 256" # parametres commun à toutes les executions 
 
 execute (){
 EXE="../prog $* $PARAM"
@@ -13,14 +13,14 @@ OUTPUT="$(echo $EXE | tr -d ' ')"
 for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
 }
 
-# execute -v 0
-# execute -v 1
-# execute -v 2
-# execute -v 3
-# execute -v 4
-# execute -v 5
+ execute -v 0
+ execute -v 1
+ execute -v 2
+ execute -v 3
+ execute -v 4
+ execute -v 5
 execute -v 6
 execute -v 7
-
+#execute -v 8
 
 
